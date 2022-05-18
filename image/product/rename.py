8f4e721e381f387b2b167854e2413f01272d7bf6
PingHuskar <61967318+PingHuskar@users.noreply.github.com>
2022-05-18 16:21:00 +0700

    add financial report using datastudio

---
+++ image/product/rename.py
@@ -0,0 +1,21 @@
+import os
+
+folder = os.getcwd()+"\\"
+count = 1
+# count increase by 1 in each iteration
+# iterate all files from a directory
+for file_name in os.listdir(folder):
+    # Construct old file name
+    source = folder + file_name
+
+    # Adding the count to the new file name and extension
+    destination = folder + file_name.replace("%","")
+
+    # Renaming the file
+    os.rename(source, destination)
+print('All Files Renamed')
+
+print('New Names are')
+# verify the result
+# res = os.listdir(folder)
+# print(res)
